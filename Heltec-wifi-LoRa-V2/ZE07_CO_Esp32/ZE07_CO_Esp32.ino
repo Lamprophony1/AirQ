410f669d74f408ec0133e67058b4932e286f94f1
Lamprophony1 <rafagarcia12@gmail.com>
2023-11-06 10:26:57 -0300

    Añadida función para lectura del sensor CO

--- Heltec-wifi-LoRa-V2/ZE07_CO_Esp32/ZE07_CO_Esp32.ino
+++ Heltec-wifi-LoRa-V2/ZE07_CO_Esp32/ZE07_CO_Esp32.ino
@@ -1,39 +1,25 @@
-/***************************************************
- ZE07 CO Sensor
-  
- ***************************************************
- This example reads the concentration of CO in air by DAC mode.
- 
- Created 2018-01-03
- By Valentin
- 
- GNU Lesser General Public License.
- See <http://www.gnu.org/licenses/> for details.
- All above must be included in any redistribution
- ****************************************************/
- 
- /***********Notice and Trouble shooting***************
- 1. This code is tested on Arduino Uno with Arduino IDE 1.0.5 r2.
- 2. In order to protect the sensor, do not touch the white sensor film on the sensor module, 
- and high concentration of Hydrogen sulfide, hydrogen, methanol, ethanol should be avoided. 
- 3. Please do not use the modules in systems which related to human being’s safety.
- ****************************************************/
-
 #include <SoftwareSerial.h>
 #include <ZE07CO_Sensor.h>
 
-#define SensorAnalogPin 37		//this pin read the analog voltage from the CO sensor
-#define VREF	2.1				//voltage on AREF pin
+#define SensorAnalogPin 37      // Este pin lee el voltaje analógico del sensor de CO
+#define VREF 2.1                // Voltaje en el pin AREF
 
-ZE07CO_Sensor ZE07CO(SensorAnalogPin,VREF);
+ZE07CO_Sensor ZE07CO(SensorAnalogPin, VREF);
 
-void setup()
-{
-    Serial.begin(9600);
+float co_ppm;  // Variable global para almacenar la concentración de CO en ppm
+
+void setup() {
+  Serial.begin(115200);
+  co_ppm = 0.0;  // Inicialización de la variable global
 }
-void loop()
-{  
-    Serial.print(ZE07CO.dacReadPPM());
-    Serial.println("ppm");
-    delay(1000);
+
+void loop() {
+  readCO_Sensor();  // Llamada a la función para leer el sensor
+  Serial.print(co_ppm);
+  Serial.println(" ppm");
+  delay(1000);
+}
+
+void readCO_Sensor() {
+  co_ppm = ZE07CO.dacReadPPM();  // Leer el sensor y almacenar el valor en la variable global
 }
